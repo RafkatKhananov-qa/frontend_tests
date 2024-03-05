10d4938080da7b6e0dda43af10ce4ffae109d4a7
Rafkat Khananov <rafkat.khananov@mail.ru>
2024-03-05 15:40:02 +0500

    add f-string to assert

--- tests/test_search_google.py
+++ tests/test_search_google.py
@@ -7,6 +7,8 @@ from pages.google_main_page import Google_main_page
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
+text_in_google = "546547657567rhffdhbfddfhdfhfdbfbbbdf"
+
 
 def test_search_google():
     # options = Options()
@@ -16,10 +18,10 @@ def test_search_google():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
     start_page = Google_main_page(driver)
-    start_page.search_information("546547657567rhffdhbfddfhdfhfdbfbbbdf")
+    start_page.search_information(text_in_google)
 
     time.sleep(3)
     text_google = driver.find_element(By.XPATH, "//p[@aria-level='3' and text()='По запросу ']")
     value_text = text_google.text
-    assert value_text == "По запросу 546547657567rhffdhbfddfhdfhfdbfbbbdf ничего не найдено. "
+    assert value_text == f"По запросу " + text_in_google + " ничего не найдено. "
     driver.quit()
